2ec7132d9a9f8085a832ad845c3004727c89bb73
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-02-18 18:22:23 -0300

    chore: initial config input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { ArrowRight, Copy } from "lucide-react";
+
 import { Button } from "@/components/button";
 import { IconButton } from "@/components/icon-button";
+import { Input } from "@/components/input";
 
 export default function Home() {
   return (
@@ -13,6 +15,10 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <Input type="email" placeholder="Digite seu e-mail" />
+      </div>
     </main>
   );
 }
