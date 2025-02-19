7f54dee6e3293b0f6c204b9e85ad00bb769af5d0
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-02-19 18:44:33 -0300

    fix: logo import

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,21 +1,28 @@
+"use client";
+
 import { Copy, Link } from "lucide-react";
 
 import { InputField, InputIcon, InputRoot } from "@/components/input";
 import { IconButton } from "@/components/icon-button";
 
-export function InviteLinkInput() {
+interface InviteLinkInputProps {
+  inviteLink: string;
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink);
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
 
-      <InputField
-        readOnly
-        defaultValue="http://localhost:3000/invite/298u128"
-      />
+      <InputField readOnly defaultValue={inviteLink} />
 
-      <IconButton className="-mr-2">
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -7,6 +7,8 @@ import { Ranking } from "./ranking";
 import { InviteLinkInput } from "./invite-link-input";
 
 export default function InvitePage() {
+  const inviteLink = "http://localhost:3000/invite/298u128";
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -33,7 +35,7 @@ export default function InvitePage() {
             </p>
           </div>
 
-          <InviteLinkInput />
+          <InviteLinkInput inviteLink={inviteLink} />
           <Stats />
         </div>
       </div>
